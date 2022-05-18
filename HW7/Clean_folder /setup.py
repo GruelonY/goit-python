0f01603a26c79409911ad94b09ceac1d17374580
Dmytriy <babenko1656@gmail.com>
2022-05-19 02:55:44 +0300

    Package for sorting files

--- HW7/Clean_folder /setup.py
+++ HW7/Clean_folder /setup.py
@@ -4,7 +4,7 @@ setup(
    name='Clean_your_files_2kkk',
    version='1.0.0',
    description='Sort files',
-   url='https://github.com/GruelonY/goit-python/blob/main/HM6/sort.py',
+   url='https://github.com/GruelonY/goit-python/blob/main/HW6/',
    author='Dima Babenko',
    author_email='babenko1656@gmail.com',
    packages=['Clean_your_files_2kkk'],
